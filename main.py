dd62a1737589ddf275835692624c9717c2a10be5
Chaostheory05 <luisbancesestrada@gmail.com>
2026-09-03 17:32:29 -0600

    se corrigio el funcionamiento de la funcion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 precedencia = {
-    '(': 1,
     '|': 2,
     '.': 3,
     '?': 4,
@@ -7,45 +6,30 @@ precedencia = {
     '+': 4,
 }
 
-
-def analizador(cadena):
-    """Esta función convierte de infix a posfix"""
-    t = list(cadena)
+def analizador(expresion):
     posfix = []
     stack = []
 
-    for current in t:
-        if current not in precedencia:
+    for current in expresion:
+        if current == '(':
+            stack.append(current)
+        elif current == ')':
+            while stack and stack[-1] != '(':
+                posfix.append(stack.pop())
+            if stack and stack[-1] == '(':
+                stack.pop()
+        elif current not in precedencia:
             posfix.append(current)
+        else:
+            while stack and stack[-1] != '(' and precedencia[stack[-1]] >= precedencia[current]:
+                posfix.append(stack.pop())
+            stack.append(current)
 
-        elif current in precedencia:
-
-
-            if len(stack) > 0:
-                if precedencia[stack[-1]] < precedencia[current]:
-                    stack.append(current)
-
-                elif precedencia[stack[-1]] > precedencia[current]:
-                    while (
-                        len(stack) > 0
-                        and precedencia[stack[-1]] > precedencia[current]
-                    ):
-                        posfix.append(stack.pop())
-
-                    stack.append(current)
-
-                else:
-                    posfix.append(stack.pop())
-                    stack.append(current)
-
-            else:
-                stack.append(current)
-
-    while len(stack) > 0:
+    while stack:
         posfix.append(stack.pop())
 
     return posfix
 
 
-c = analizador("a|b")
+c = analizador("(b|b)*abb(a|b)*")
 print(c)
